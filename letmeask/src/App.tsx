6392ffa3d312da0a27f942b185b0b4ea9dded7fb
Leandro <fahurleandro@gmail.com>
2021-06-25 18:09:27 -0700

    newRoom page

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Home } from './pages/Home';
+// import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
-  return <Home />;
+  // return <Home />;
+  return <NewRoom />;
 }
 
 export default App;
---
+++ letmeask/src/pages/NewRoom.tsx
@@ -0,0 +1,31 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  return (
+    <div id='page-auth'>
+      <aside>
+        <img src={illustrationImg} alt='illustration for Q&A' />
+        <strong>Crie salas de Q&ampA; ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tmepo-real</p>
+      </aside>
+      <main>
+        <div className='main-content'>
+          <img src={logoImg} alt='Letmeask' />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input type='text' placeholder='nome da sala' />
+            <Button type='submit'>Criar a sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <a href='#'>clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
