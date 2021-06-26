0d90210872cf08ebf0174a581f72ebbf1898cc95
Leandro <fahurleandro@gmail.com>
2021-06-25 18:24:41 -0700

    Router

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-// import { Home } from './pages/Home';
+import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   // return <Home />;
-  return <NewRoom />;
+  // return <NewRoom />;
+  return (
+    <BrowserRouter>
+      <Route exact path='/' component={Home} />
+      <Route path='/rooms/new' component={NewRoom} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useHistory } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleImg from '../assets/images/google-icon.svg';
@@ -6,6 +7,12 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
 export function Home() {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push('/rooms/new');
+  }
+
   return (
     <div id='page-auth'>
       <aside>
@@ -16,14 +23,16 @@ export function Home() {
       <main>
         <div className='main-content'>
           <img src={logoImg} alt='Letmeask' />
-          <Button className='create-room'>
+          <Button className='create-room' onClick={navigateToNewRoom}>
             <img src={googleImg} alt='logo do google' />
             Crie sua sala com o Google
           </Button>
           <div className='separator'>ou entre em uma sala</div>
           <form>
             <input type='text' placeholder='digite o código da sala' />
-            <Button type='submit'>Entrar na sala</Button>
+            <Button type='submit' onClick={navigateToNewRoom}>
+              Entrar na sala
+            </Button>
           </form>
         </div>
       </main>
